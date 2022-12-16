2d299400e6e41857a1ad3352ed93736d8a9d3dfb
alixbarreaux <barreaux.alix@kisquadstudio.com>
2022-12-16 14:03:25 +0100

    Rescale cloud textures, scale Cloud scene to 1 ratio, add visibility notifier performance

--- Assets/Textures/Ambient/Cloud.gd
+++ Assets/Textures/Ambient/Cloud.gd
@@ -3,20 +3,51 @@ extends Sprite
 
 # --------------------------    DECLARE VARIABLES     --------------------------
 
+
 var textures : PoolStringArray = ["res://Assets/Textures/Ambient/cloud.png", "res://Assets/Textures/Ambient/cloud_2.png", "res://Assets/Textures/Ambient/cloud_3.png"]
 
 var random_texture_picker = RandomNumberGenerator.new()
 var current_texture
 
+
+# Node References
+onready var visibility_notifier_2d: VisibilityNotifier2D = $VisibilityNotifier2D
+
+
 # ----------------------------    RUN THE CODE     -----------------------------
 
-func _ready() -> void:
-	pick_random_texture()
 
-func _physics_process(delta : float) -> void:
-	pass
+func _ready() -> void:
+	self._initialize_signals()
+	self._initialize()
+
 
 # --------------------------    DECLARE FUNCTIONS     --------------------------
+
+
+func _initialize_signals() -> void:
+	visibility_notifier_2d.connect("screen_entered", self, "on_visibility_notifier_2d_screen_entered")
+	visibility_notifier_2d.connect("screen_exited", self, "on_visibility_notifier_2d_screen_exited")
+	return
+
+
+func _initialize() -> void:
+	pick_random_texture()
+	self.hide()
+	return
+
+
+func on_visibility_notifier_2d_screen_entered() -> void:
+	print("Show")
+	self.show()
+	return
+
+
+func on_visibility_notifier_2d_screen_exited() -> void:
+	print("Hide")
+	self.hide()
+	return
+
 
 func pick_random_texture() -> void:
 	random_texture_picker.randomize()
